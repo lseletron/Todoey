926b5d1956cd64452a131e559f299959b0df506e
Luciano da Silva <luciano.engenharia@soniclear.com.br>
2018-11-18 18:26:23 -0200

    Convert to MVC design pattern.

---
+++ Todoey/Data Model/Item.swift
@@ -0,0 +1,14 @@
+//
+//  Item.swift
+//  Todoey
+//
+//  Created by Luciano da Silva on 17/11/18.
+//  Copyright © 2018 Luciano da Silva. All rights reserved.
+//
+
+import Foundation
+
+class Item  {
+    var title: String = ""
+    var done: Bool = false
+}
